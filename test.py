a8fab3fa714149b5b14505516ba75dbb44e2a711
Deeksha <deekshakavthar@gmail.com>
2026-06-25 16:56:15 +0530

    git commands

---
+++ test.py
@@ -0,0 +1,26 @@
+print("Hello world","world",sep="#")
+
+x="10.5"
+
+print("abc" > "abd")
+
+print("\test\ntesting")
+print(r"\test\ntesting")
+print("\\test\\ntesting")
+
+print("""hello
+      my name is deekshaa""")
+
+x="hello"
+print(x[0:3:2])
+print(x[::-1])
+
+l=[1,2,3,4,5]
+print(l[::2])
+
+for i in range(1,10,2):
+    print(i)
+
+for i in range(5):
+    print("hello")
+    
